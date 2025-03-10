031a5c7ae4b11bc2e0ad5e71eeadce3e17e4fe6a
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-10 10:04:23 -0500

    added some styling

--- 01-starting-project-angular/src/app/app.component.ts
+++ 01-starting-project-angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component"; // unlocked through imports
+
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component"; // unlocked through imports
 
 @Component({ // this is a 'decorator'; a ts feature that adds metadata to the thing its attached to. Angular uses this to add extra metadata to the class. We are importing this from the Angular framework core package.
   selector: 'app-root', // tells Angular what elements it should look for so they can be replaced by this component and its markup (stored in templateUrl).
   standalone: true,
-  imports: [HeaderComponent], // works together with the standalone property.
+  imports: [HeaderComponent, UserComponent], // works together with the standalone property.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
